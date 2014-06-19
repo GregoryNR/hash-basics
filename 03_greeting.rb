c412b385ba7303696280a0013bd7fca9ceb8a2a2
Natalie Gregory <gregorynr@gmail.com>
2014-06-19 14:31:39 -0600

    nickname

--- 03_greeting.rb
+++ 03_greeting.rb
@@ -1,4 +1,4 @@
 require_relative "person"
 
 # Print the string "Hello, nice to meet you Bjorn Borg."
-print "Hello, nice to meet you #{BJORN_BORG["first name"]}
+puts "Hello, nice to meet you #{BJORN_BORG["first_name","last_name"]}
